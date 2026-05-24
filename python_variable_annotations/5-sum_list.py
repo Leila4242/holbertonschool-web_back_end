a0407792cd83a60fde052cca3335f8c79f3f1b6a
Leila4242 <aliyevaleyla6277@gmail.com>
2026-05-24 18:28:30 +0400

    done

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Module that defines a list of floats."""
+from typing import List
+
+# Defining and annotating the function
+def sum_list(input_list: List[float])
+    sum = 0
+    for i in input_list:
+        sum+= i 
+    return sum 
+   
