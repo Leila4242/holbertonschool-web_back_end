85fee1583f57ee0cd8c0a6a0ac55c12a7a4dcf05
Leila4242 <aliyevaleyla6277@gmail.com>
2026-05-24 18:32:54 +0400

    done

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,10 +3,10 @@
 """Module that defines a list of floats."""
 from typing import List
 
+
 # Defining and annotating the function
-def sum_list(input_list: List[float])-> float:
+def sum_list(input_list: List[float]) -> float:
     sum = 0
     for i in input_list:
-        sum+= i 
-    return sum 
-   
+        sum += i 
+    return sum
