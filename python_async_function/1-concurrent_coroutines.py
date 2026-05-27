477fb58afac65fbdb1fd25f2f55596feff6408a6
Leila4242 <aliyevaleyla6277@gmail.com>
2026-05-27 16:39:53 +0400

    zipzipzip

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -18,11 +18,14 @@ wait_random = basic_async_syntax.wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run `n` coroutines concurrently that await `wait_random(max_delay)`.
     """
-    # 1. n sayda tapşırığı (coroutine obyektini) siyahıya yığırıq (hələ ki işə düşmürlər)
+    # 1. n sayda tapşırığı (coroutine obyektini)
+    # siyahıya yığırıq (hələ ki işə düşmürlər)
     tasks = [wait_random(max_delay) for _ in range(n)]
     
-    # 2. Ulduz (*) işarəsi ilə siyahını açırıq və asyncio.gather ilə HAMISINI EYNI ANDA başladırıq
+    # 2. Ulduz (*) işarəsi ilə siyahını açırıq və
+    # asyncio.gather ilə HAMISINI EYNI ANDA başladırıq
     delays = await asyncio.gather(*tasks)
     
-    # 3. Bizdən nəticələrin artan sıra ilə qaytarılması tələb olunur (ascending order)
+    # 3. Bizdən nəticələrin artan sıra ilə qaytarılması
+    # tələb olunur (ascending order)
     return sorted(delays)
