125a370701ca0b370c3f361fd94320c700c0cf47
Leila4242 <aliyevaleyla6277@gmail.com>
2026-06-13 23:49:05 +0400

    done

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-"""wait_n module"""
-
+"""Module for task-based concurrent coroutines."""
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times and return delays in ascending order."""
-    delays = await asyncio.gather(
-        *(wait_random(max_delay) for _ in range(n))
-    )
-    return sorted(delays)
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn task_wait_random n times and return delays in ascending order."""
+    delays = []
+
+    async def add_delay():
+        delay = await task_wait_random(max_delay)
+        i = 0
+        while i < len(delays) and delays[i] < delay:
+            i += 1
+        delays.insert(i, delay)
+
+    await asyncio.gather(*[add_delay() for _ in range(n)])
+    return delays
